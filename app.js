e27edf65ea875d748aae57f00af3e6c747acfc3d
devarshil <arshil.jbs@gmail.com>
2024-03-01 09:52:29 +0530

    some chenges

--- app.js
+++ app.js
@@ -1,30 +1,63 @@
+// const connectDB = require("./config/db");
+// const express = require("express");
+// const cors = require("cors");
+// const appRouter = require("./routes");
+// const { mkdirSync } = require("fs");
+// const { UPLOADS_DIR,IMAGE_DIR } = require("./config");
+
+// connectDB()
+
+// mkdirSync(UPLOADS_DIR, { recursive: true });
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use("/uploads", express.static(UPLOADS_DIR));
+// app.use(express.static('public'));
+// app.set('view engine', 'ejs');
+
+// app.use("/api", appRouter);
+
+// app.use('/',(req, res, next) => {
+//     res.status(404).json({
+//         error: 'Not Found',
+//         message: 'Something went wrong!'
+//     });
+// });
+
+
+// module.exports = app;
+
 const connectDB = require("./config/db");
 const express = require("express");
 const cors = require("cors");
 const appRouter = require("./routes");
 const { mkdirSync } = require("fs");
-const { UPLOADS_DIR,IMAGE_DIR } = require("./config");
+const { UPLOADS_DIR, IMAGE_DIR } = require("./config");
 
-connectDB()
+connectDB();
 
 mkdirSync(UPLOADS_DIR, { recursive: true });
 
 const app = express();
+
+// Set up static middleware for serving images
+app.use("/uploads", express.static(UPLOADS_DIR));
+app.use(express.static('assets'));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/uploads", express.static(UPLOADS_DIR));
-app.use(express.static('assets'));
 app.set('view engine', 'ejs');
 
 app.use("/api", appRouter);
 
-app.use('/',(req, res, next) => {
+app.use('/', (req, res, next) => {
     res.status(404).json({
         error: 'Not Found',
         message: 'Something went wrong!'
     });
 });
-
 
 module.exports = app;
