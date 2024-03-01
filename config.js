1b3b7af1155f950964abe1f418fe0d772795cc16
devarshil <arshil.jbs@gmail.com>
2024-03-01 15:17:55 +0530

    add /

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports.ROOT_DIR = this.IS_EXECUTABLE
     ? process.cwd()
     : path.dirname(path.resolve(__dirname));
 module.exports.LOG_DIR = path.join(this.ROOT_DIR, "assets");
-module.exports.UPLOADS_DIR = path.join(this.LOG_DIR, "images");
+module.exports.UPLOADS_DIR = path.join(this.LOG_DIR, "images/");
 
 require("dotenv").config(
     isExecutable
