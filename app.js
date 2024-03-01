1c0f7b54a9a6e0a2d226f3394eafe4134d52705f
devarshil <arshil.jbs@gmail.com>
2024-03-01 09:17:49 +0530

    some chenge in app.js file

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/uploads", express.static(UPLOADS_DIR));
-app.use(express.static('public'));
+app.use(express.static('assets'));
 app.set('view engine', 'ejs');
 
 app.use("/api", appRouter);
