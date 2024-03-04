0129e50c608f4e06bf6a8f09bf7c0fff1cabcdda
devarshil <arshil.jbs@gmail.com>
2024-03-04 17:35:29 +0530

    some chenges in app file...

--- app.js
+++ app.js
@@ -2,13 +2,8 @@ const connectDB = require("./config/db");
 const express = require("express");
 const cors = require("cors");
 const appRouter = require("./routes");
-const { mkdirSync } = require("fs");
-const { UPLOADS_DIR,IMAGE_DIR } = require("./config");
 const bodyParser = require('body-parser');
 connectDB()
-
-mkdirSync(UPLOADS_DIR, { recursive: true });
-
 const app = express();
 // app.use(cors());
 const corsConfig = {
@@ -20,7 +15,6 @@ app.use(cors(corsConfig))
 app.options("", cors(corsConfig))
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/uploads", express.static(UPLOADS_DIR));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
